1c1b3f0c6d23a1d923c42bfdc87bd53fd0869def
srikeerthis <srikeerthisrinivasan@gmail.com>
2020-07-19 21:53:22 +0530

    Added new routes to check login status and display content accordingly

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authRoutes = require("./routes/auth-routes");
+const profileRoutes = require("./routes/profile-routes");
 const passportSetup = require("./config/passport-setup");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
@@ -32,11 +33,13 @@ mongoose.connect(keys.mongodb.mongoURI, () => {
 
 //set up routes
 app.use("/auth", authRoutes);
+app.use("/profile", profileRoutes);
 
 //create homepage
 
 app.get("/", (req, res) => {
-  res.render("home");
+  //send object to check status and display content
+  res.render("home", { user: req.user });
 });
 
 app.listen(3000, () => {

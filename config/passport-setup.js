8e441af0770ce39e14beb133fc36872955a010ad
srikeerthis <srikeerthisrinivasan@gmail.com>
2020-07-22 16:21:37 +0530

    Added twitter and facebook stratergy

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,6 +2,7 @@ const passport = require("passport");
 const githubStatergy = require("passport-github");
 const googleStatergy = require("passport-google-oauth20");
 const twitterStatergy = require("passport-twitter");
+const facebookStatergy = require("passport-facebook");
 
 const keys = require("./keys");
 const User = require("../models/user-model");
@@ -68,9 +69,38 @@ passport.use(
   new twitterStatergy(
     {
       //option for github stratergy
-      clientID: keys.twitter.clientID,
-      clientSecret: keys.twitter.clientSecret,
+      consumerKey: keys.twitter.consumerKey,
+      consumerSecret: keys.twitter.consumerSecret,
       callbackURL: "/auth/twitter/cb",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      //check if user exists
+      User.findOne({ oauthId: profile.id }).then((currentUser) => {
+        if (currentUser) {
+          done(null, currentUser);
+        } else {
+          new User({
+            username: profile.displayName,
+            oauthId: profile.id,
+          })
+            .save()
+            .then((newUser) => {
+              console.log("user data created");
+              done(null, newUser);
+            });
+        }
+      });
+    }
+  )
+);
+
+passport.use(
+  new facebookStatergy(
+    {
+      //option for github stratergy
+      clientID: keys.facebook.clientID,
+      clientSecret: keys.facebook.clientSecret,
+      callbackURL: "/auth/facebook/cb",
     },
     (accessToken, refreshToken, profile, done) => {
       //check if user exists
